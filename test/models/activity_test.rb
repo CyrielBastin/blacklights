515faba6afd1b8d125e37d367c19b407a70ded19
Grimm <cyriel.bastin@zoho.com>
2021-03-01 15:47:54 +0100

    further testing of 'Activity' model

--- test/models/activity_test.rb
+++ test/models/activity_test.rb
@@ -2,39 +2,51 @@ require "test_helper"
 
 class ActivityTest < ActiveSupport::TestCase
 
-  test "Should not save activity without a name" do
+  test 'Should not save activity without a name' do
     activity = Activity.new
     assert_not activity.save
   end
 
-  test "Should not save activity without a description" do
+  test 'Should not save activity without a description' do
     activity = Activity.new
-    activity.name = "Badminton"
+    activity.name = 'Badminton'
     assert_not activity.save
   end
 
-  test "Name should contain at least 4 characters" do
+  test 'Name should contain at least 4 characters' do
     activity = Activity.new
-    activity.name = "one"
-    activity.description = "This is a description !!!"
+    activity.name = 'one'
+    activity.description = 'This is a description !!!'
     assert_not activity.save
   end
 
-  test "Description should contain at least 15 characters" do
+  test 'Description should contain at least 15 characters' do
     activity = Activity.new
-    activity.name = "Four"
-    activity.description = "This is a desc"  # 14 chars
+    activity.name = 'Four'
+    activity.description = 'This is a desc'  # 14 chars
     assert_not activity.save
   end
 
-  test "Badminton's name" do
+  test 'Badminton\'s name' do
     activity = activities(:badminton)
-    assert_equal("Badminton", activity.name)
+    assert_equal('Badminton', activity.name)
   end
 
-  test "Basketball's description" do
+  test 'Basketball\'s description' do
     activity = activities(:basketball)
-    assert_equal "Le basketball, ça étonne!", activity.description
+    assert_equal 'Le basketball, ça étonne!', activity.description
+  end
+
+  test 'Should update Badminton\'s name' do
+    activity = activities(:badminton)
+    activity.update({ name: 'Climbing' })
+    activity = activities(:badminton)
+    assert_equal 'Climbing', activity.name
+  end
+
+  test 'Should delete Badminton' do
+    activity = activities(:badminton)
+    assert activity.delete
   end
 
 end
