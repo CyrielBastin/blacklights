c0fac91e482e5448aaebc90cda9b236e886e35b1
Grimm <cyriel.bastin@zoho.com>
2021-03-01 19:14:38 +0100

    tests and validations for 'Dimension' model

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -2,4 +2,9 @@ class Dimension < ApplicationRecord
 
   has_one :equipment
   has_one :location
+
+  validates :width, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :length, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :height, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :weight, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
 end
