40d71168ac9571ff2b57fb2eda4b866e7a86bdc4
Grimm <cyriel.bastin@zoho.com>
2021-03-02 23:22:30 +0100

    Tests and validations for 'Supplier' model

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,4 +3,10 @@ class Supplier < ApplicationRecord
   belongs_to :contact
   has_many :equipments
 
+  min_char_name = 4
+  err_msg = { name_not_blank: 'Le nom ne peut pas être vide',
+              name_too_short: "La description doit contenir au moins #{min_char_name} caractères" }
+  validates :name, presence: { message: err_msg[:name_not_blank] },
+                   length: { minimum: 4, message: err_msg[:name_too_short] }
+
 end
---
+++ test/models/supplier_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class SupplierTest < ActiveSupport::TestCase
+
+  test 'Sould not save supplier without a name' do
+    supplier = Supplier.new
+    supplier.contact = contacts(:one)
+    assert_not supplier.save
+  end
+
+  test 'Name should be at least 4 characters long' do
+    supplier = Supplier.new
+    supplier.contact = contacts(:one)
+    supplier[:name] = 'Bob'
+    assert_not supplier.save
+  end
+
+  test 'One\'s contact\'s firstname should be Cain' do
+    supplier = suppliers(:one)
+    assert_equal 'Cain', supplier.contact[:firstname]
+  end
+
+  test 'Two\'s contact\'s coordinate\'s city should be Namur' do
+    supplier = suppliers(:two)
+    assert_equal 'Namur', supplier.contact.coordinate[:city]
+  end
+
+end
