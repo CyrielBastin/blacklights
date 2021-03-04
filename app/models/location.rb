247ee43038ecc612eb2a16c463a5b5d2ba8ae397
Grimm <cyriel.bastin@zoho.com>
2021-03-04 21:19:04 +0100

    Tests for Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,12 @@
 class Location < ApplicationRecord
 
-
   belongs_to :contact
   belongs_to :dimension
   has_many :location_activities
   has_many :events
+
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+  validates :type, presence: true, length: { minimum: 7 }
 
   def self.inheritance_column
     nil
