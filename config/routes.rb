3317a55725ed91fa274e4d39d34b1e1ab7a358b8
Grimm <cyriel.bastin@zoho.com>
2021-04-13 11:24:07 +0200

    Change get to patch for registration confirm

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     resources :suppliers, concerns: :paginatable
     post 'import_suppliers', to: 'suppliers#import'
     resources :registrations, concerns: :paginatable do
-      get 'confirm'
+      patch 'confirm'
     end
     post 'import_registrations', to: 'registrations#import'
     get '/json/location_activities/:loc_id', to: 'activities#location_activities_json'
