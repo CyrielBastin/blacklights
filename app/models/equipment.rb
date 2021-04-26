b04d38e0aaa24c1f5805b69576da3001ded9e364
Grimm <cyriel.bastin@zoho.com>
2021-04-27 00:26:16 +0200

    french repr for price in equipment

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -25,10 +25,8 @@ class Equipment < ApplicationRecord
 
   min_char_name = 5
   min_char_description = 10
-  min_unit_price = 0.00
   ERR_MSG = { name_is_too_short: "doit contenir au moins #{min_char_name} caractères",
-              description_is_too_short: "doit contenir au moins #{min_char_description} caractères",
-              unit_price_is_too_low: "doit être supérieur à #{min_unit_price}€" }.freeze
+              description_is_too_short: "doit contenir au moins #{min_char_description} caractères" }.freeze
 
   validates :name, presence: true,
                    length: { minimum: min_char_name, message: ERR_MSG[:name_is_too_short] }
@@ -36,8 +34,7 @@ class Equipment < ApplicationRecord
   validate :name_is_valid
   validates :description, presence: true,
                           length: { minimum: min_char_description, message: ERR_MSG[:description_is_too_short] }
-  validates :unit_price, presence: true,
-                         numericality: { greater_than: min_unit_price, message: ERR_MSG[:unit_price_is_too_low] }
+  validates :unit_price, presence: true
 
 
   def name_is_valid
