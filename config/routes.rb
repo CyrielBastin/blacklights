caa25ae0ed7f6d6b64a55ded75feab5625059513
sma-majconsulting <sma@maj.consulting>
2021-04-30 14:02:27 +0200

    begin of refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,7 @@ Rails.application.routes.draw do
   root to: 'public#index'
 
   get "activities", to: "public#activities"
+  resources :events
   get "events", to: "public#events"
   get "about", to: "public#about"
   get "contact", to: "public#contact"
