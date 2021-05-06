28982fcea601931d70657a0c55ce13e88801fc9c
Cyriel Bastin <cyriel.bastin@zoho.com>
2021-05-06 15:53:46 +0200

    fix export user

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,10 +3,31 @@ module UserHelper
   # Each user must complete their profile before they can register for an event.
   # This method checks if +user+ has completed their profile and are eligible to partake in an event.
   #
-  # @param [ActiveRecord::User] user
+  # @param [ApplicationRecord::User] user
   # @return [Boolean]
   def registration_eligible?(user)
     user.profile[:birthdate].present? && user.contact.coordinate.present?
+  end
+
+  # == Pack all roles for a user into a +String+
+  # @example
+  #  user = User.find 1
+  #  pack_roles_to_fr user
+  #  -> "Organisateur - Admin"
+  #
+  #  if user.roles.empty?, returns "Utilisateur"
+  #
+  # @param [ApplicationRecord::User] user
+  # @return [String]
+  def pack_roles_to_fr(user)
+    return 'Utilisateur' if user.roles.empty?
+
+    str = ''
+    str += ' - Organisateur' if user.has_role? :organizer
+    str += ' - Fournisseur' if user.has_role? :supplier
+    str += ' - Admin' if user.has_role? :admin
+
+    str[3..]
   end
 
 end
